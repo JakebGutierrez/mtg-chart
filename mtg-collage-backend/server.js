8e189834e5a38e74ba53fe46334deaa213e438e2
Jakeb Gutierrez <drwaffle69@gmail.com>
2023-11-01 20:22:29 +1100

    added basic search functionality

--- mtg-collage-backend/server.js
+++ mtg-collage-backend/server.js
@@ -16,6 +16,19 @@ app.get('/card/:name/:set', async (req, res) => {
     }
 });
 
+app.get('/search/:query', async (req, res) => {
+    try {
+        const response = await axios.get(`https://api.scryfall.com/cards/search?q=${req.params.query}`);
+        if (response.data && response.data.data) {
+            res.json(response.data.data); // Send the array of cards.
+        } else {
+            res.json([]);
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Error searching for cards" });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
