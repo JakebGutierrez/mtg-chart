563108443e5a73285942546fc3961a490741fc0b
Jakeb Gutierrez <drwaffle69@gmail.com>
2023-11-01 18:49:45 +1100

    Initial commit

---
+++ mtg-collage-backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/card/:name/:set', async (req, res) => {
+    try {
+        const response = await axios.get(`https://api.scryfall.com/cards/named?set=${req.params.set}&fuzzy=${req.params.name}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: "Card not found" });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
