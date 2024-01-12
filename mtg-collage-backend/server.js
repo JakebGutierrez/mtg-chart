6f043b37bfc52e1e1dea1524630705e4051989fb
Jakeb Gutierrez <drwaffle69@gmail.com>
2024-01-12 18:12:31 +1100

    fly io config

--- mtg-collage-backend/server.js
+++ mtg-collage-backend/server.js
@@ -8,11 +8,11 @@ const app = express();
 const corsOptions = {
     origin: process.env.NODE_ENV === 'production' 
       ? 'https://mtgchart.netlify.app/' // Netlify frontend URL
-      : 'http://localhost:3000', // Frontend URL
+      : 'http://localhost:3000', // Local frontend URL for development
     optionsSuccessStatus: 200
-  };
-  
-  app.use(cors(corsOptions));
+};
+
+app.use(cors(corsOptions));
 
 app.get('/card/:name/:set', async (req, res) => {
     try {
@@ -37,8 +37,13 @@ app.get('/search/:query', async (req, res) => {
 });
 
 // Only listen on a port if not running in a serverless environment
-if (!process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
-  }
+// if (!process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+//     app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
+//   }
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
 
 module.exports.handler = serverless(app);
