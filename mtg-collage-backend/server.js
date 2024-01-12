b4b112d52be3b1cc58b70cfd698642b39c86de28
Jakeb Gutierrez <drwaffle69@gmail.com>
2024-01-12 19:12:07 +1100

    undo external backend config

--- mtg-collage-backend/server.js
+++ mtg-collage-backend/server.js
@@ -1,19 +1,18 @@
-const serverless = require('serverless-http');
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
 
+// CORS configuration for local development
 const corsOptions = {
-    origin: process.env.NODE_ENV === 'production' 
-      ? 'https://mtgchart.netlify.app/' // Netlify frontend URL
-      : 'http://localhost:3000', // Local frontend URL for development
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
 
+// Route for getting a card by name and set
 app.get('/card/:name/:set', async (req, res) => {
     try {
         const response = await axios.get(`https://api.scryfall.com/cards/named?set=${req.params.set}&fuzzy=${req.params.name}`);
@@ -23,27 +22,17 @@ app.get('/card/:name/:set', async (req, res) => {
     }
 });
 
+// Route for searching cards
 app.get('/search/:query', async (req, res) => {
     try {
         const response = await axios.get(`https://api.scryfall.com/cards/search?q=${req.params.query}`);
-        if (response.data && response.data.data) {
-            res.json(response.data.data); // Send the array of cards.
-        } else {
-            res.json([]);
-        }
+        res.json(response.data.data || []);
     } catch (error) {
         res.status(500).json({ message: "Error searching for cards" });
     }
 });
 
-// Only listen on a port if not running in a serverless environment
-// if (!process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-//     app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
-//   }
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// Start the server on port 3000
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
-
-module.exports.handler = serverless(app);
